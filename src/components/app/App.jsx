c1ea245eccaeeba97ffef9ed447ac01c82553bae
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-05-17 01:48:22 +0200

    add all components

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,22 +1,21 @@
 import { Component } from "react";
-// import s from '../app/App.module.css';
-// import { nanoid } from "nanoid";
+
 
 class App extends Component {
 
-state = {
 
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
 }
 
-  render() {
-   
-    return (
-      <div >
-      
-      </div>
-    );
-  }
-  
-};
+
+
+
 
 export default App;
