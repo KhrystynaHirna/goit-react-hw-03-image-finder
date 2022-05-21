c90dd63f0c8edf3d03dde584fac59a94656d39fe
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-05-21 17:33:53 +0200

    Update Loader.jsx

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,15 +1,15 @@
-// import PropTypes from "prop-types";
-// import { SpinnerDiamond } from "spinners-react";
-// import s from "./Loader.module.css";
+import PropTypes from "prop-types";
+import { SpinnerDiamond } from "spinners-react";
+import s from "./Loader.module.css";
 
 
-// export function Loader({ enabled }) {
-//     return (
-//         <div className={s.Loader}>
-//             <SpinnerDiamond enabled={enabled} />
-//         </div>
-//     )
-// };
-// Loader.propTypes = {
-//     enabled: PropTypes.bool.isRequired,
-// };
+export function Loader({ enabled }) {
+    return (
+        <div className={s.Loader}>
+            <SpinnerDiamond enabled={enabled} />
+        </div>
+    )
+};
+Loader.propTypes = {
+    enabled: PropTypes.bool.isRequired,
+};
